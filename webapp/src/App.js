3af6381328d8734b4ad34419990618bb7198c79e
Melvin Macaranas <greatwinblowaway@gmail.com>
2020-10-01 19:18:21 +0700

    create form

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Form } from "react-bootstrap";
+import "./App.css";
 
 function App() {
+
+  const [name, setName] = useState('');
+
+  const submitName = () => {
+    // TODO: submit name
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="form">
+        <Form>
+          <Form.Group controlId="forBasicName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter name" value={name} onChange={(e) => setName(e.target.value)} />
+            <Form.Text className="text-muted">
+              We'll never share your name with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
